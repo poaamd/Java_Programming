9ccb41de036970f17b9edcfd4d8a893f13f890b5
Pouya <poa.amd@gmail.com>
2023-09-25 20:27:48 -0400

    add week3 class

---
+++ Week3/src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    //properties
+    private int id;
+    private String name;
+    private String address;
+    //static property - static variables
+    public static String province;
+    private double salary;
+
+    //parameterized constructor
+    
+}
